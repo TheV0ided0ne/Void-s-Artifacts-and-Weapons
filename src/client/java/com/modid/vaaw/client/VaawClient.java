ea99e0b6e21245a3f541d3a9b28136950de050d3
TheV0ided0ne <chrislastguest@gmail.com>
2025-12-03 14:56:34 +1100

    Initial commit - Minecraft 1.20.1 mod

---
+++ src/client/java/com/modid/vaaw/client/VaawClient.java
@@ -0,0 +1,10 @@
+package com.modid.vaaw.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VaawClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/modid/vaaw/Vaaw.java
@@ -0,0 +1,10 @@
+package com.modid.vaaw;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Vaaw implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
