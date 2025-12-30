5d6ae81ceca3dce76442ba1b22ab6c976b415ac6
nuxl0x <nuclearx86@gmail.com>
2025-12-30 16:40:02 +1100

    Recipe Generation.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 A mod based upon a show in the making called 'Into The Multiverse' that will contain items to help you venture through a difficult modpack and world that is introduced within the modpack "Legend of The Voided One: Reborn!"
 
----Credits--
+---Credits---
 
 VoidVA - Creator of ITM and modpack / modpack coder
 
--- src/main/kotlin/vee/vaaw/VaawDataGenerator.kt
+++ src/main/kotlin/vee/vaaw/VaawDataGenerator.kt
@@ -6,20 +6,20 @@ import vee.vaaw.datagen.DamageTypeProvider
 import vee.vaaw.datagen.DamageTypeTagProvider
 import vee.vaaw.datagen.EnglishLanguageProvider
 import vee.vaaw.datagen.ItemModelProvider
+import vee.vaaw.datagen.recipe.IngotRecipeProvider
+import vee.vaaw.datagen.recipe.MaterialRecipeProvider
 
 object VaawDataGenerator : DataGeneratorEntrypoint {
 
     override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
         val pack = fabricDataGenerator.createPack()
 
-
+        // Provider List
         pack.addProvider(::EnglishLanguageProvider)
-
         pack.addProvider(::DamageTypeProvider)
-
         pack.addProvider(::DamageTypeTagProvider)
-
         pack.addProvider(::ItemModelProvider)
+        pack.addProvider(::MaterialRecipeProvider)
 
     }
 
