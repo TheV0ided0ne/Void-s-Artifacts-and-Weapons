f524229febd2fd9d9a425b8c86570396374f3f02
nuxl0x <nuclearx86@gmail.com>
2026-04-27 16:58:44 +1000

    Improvements to data generation backend.
    # Conflicts:
    #	src/main/kotlin/vee/vaaw/datagen/EnglishLanguageProvider.kt

---
+++ src/main/kotlin/vee/vaaw/datagen/EnglishLanguageMaps.kt
@@ -0,0 +1,53 @@
+package vee.vaaw.datagen
+
+import vee.vaaw.util.text.MessageText
+import vee.vaaw.util.text.TooltipText
+
+object EnglishLanguageMaps {
+
+    val itemGroups = mapOf(
+        "itemGroup.vaaw_armour" to "VAAW Armour",
+        "itemGroup.vaaw_consumables" to "VAAW Consumables",
+        "itemGroup.vaaw_materials" to "VAAW Materials",
+        "itemGroup.vaaw_weapons" to "VAAW Weapons",
+        "itemGroup.vaaw_misc" to "VAAW Miscellaneous"
+    )
+
+    val trinketSlots = mapOf(
+        "trinkets.slot.vaaw.trinket_1" to "Artifact Slot 1",
+        "trinkets.slot.vaaw.trinket_2" to "Artifact Slot 2",
+        "trinkets.slot.vaaw.trinket_3" to "Artifact Slot 3",
+    )
+
+    val effects = mapOf(
+        "effect.vaaw.void_touched" to "Void Touched",
+        "effect.vaaw.angelic_injection" to "Angelic Injection"
+    )
+
+    val messages = mapOf(
+        "death.attack.void_touched" to MessageText.VoidTouched.DEATH,
+        "death.attack.void_touched.player" to MessageText.VoidTouched.DEATH_PLAYER,
+        "death.attack.bleed" to MessageText.Bleed.DEATH,
+        "death.attack.bleed.player" to MessageText.Bleed.DEATH_PLAYER
+    )
+
+    val tooltips = mapOf(
+        "itemTooltip.vaaw.void_scythe1" to TooltipText.VoidScythe.ONE,
+        "itemTooltip.vaaw.void_scythe2" to TooltipText.VoidScythe.TWO,
+        "itemTooltip.vaaw.oblivion_scythe1" to TooltipText.OBLIVION_SCYTHE
+    )
+
+    val keybinds = mapOf(
+        "key.vaaw.activate_armour_ability" to "Activate Armour Ability",
+        "key.vaaw.activate_weapon_ability" to "Activate Weapon Ability"
+    )
+
+    val categories = mapOf(
+        "category.vaaw.general" to "VAaW Keybinds"
+    )
+
+    fun getMaps(): List<Map<String, String>> {
+        return listOf(itemGroups, trinketSlots, effects, messages, keybinds, tooltips, categories)
+    }
+
+}
---
+++ src/main/kotlin/vee/vaaw/util/text/TooltipText.kt
@@ -0,0 +1,12 @@
+package vee.vaaw.util.text
+
+object TooltipText {
+
+    object VoidScythe {
+        const val ONE = "§5Forged by The Voided One himself to fulfill a purpose after his Oblivion Scythe shattered in ITM."
+        const val TWO = "§5now §4YOU §5must carry this purpose and defeat all who stand in your way with this legendary weapon!"
+    }
+
+    const val OBLIVION_SCYTHE = "The start of a legend, use this scythe that 'The Voided One' himself used to shatter all evil in your way!"
+
+}
